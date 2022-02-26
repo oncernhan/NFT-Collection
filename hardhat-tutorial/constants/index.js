9567a7aa01aa6db96fdead6bb0c34f3771df4b86
Phan <pha101@csiro.au>
2022-02-26 23:12:40 +1000

    initial commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+//// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xD673639A73e722396b39Bd0Be234Ff5f824b60Ee";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
